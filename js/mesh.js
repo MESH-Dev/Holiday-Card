a1460cb16159f01f9afe45120e3e66fbcb55c866
robbiesh <sherrard.rob@gmail.com>
2014-12-22 17:39:37 -0500

    first commit

---
+++ js/mesh.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    $('.menu-description').hide();
+    
+    $('.menu-item').click(function() {
+        $(this).find('.menu-description').slideToggle();
+        $('.action-item').hide();
+        $action = $(this).find('.menu-description').data('action-type');
+        $('.' + $action).show();
+    });
+    
+    $('.rain').click(function() {
+        $(document).snowfall({image :"images/MESH.png", minSize: 10, maxSize:32});
+    });
+    
+});
