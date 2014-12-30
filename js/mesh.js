9cbeace448c0ff2e6bef0bd72feef8a1fd8d1823
robbiesh <sherrard.rob@gmail.com>
2014-12-30 18:28:42 -0500

    final touches

--- js/mesh.js
+++ js/mesh.js
@@ -1,9 +1,43 @@
+(function($){
+
+  $.fn.shuffle = function() {
+
+    var allElems = this.get(),
+    getRandom = function(max) {
+      return Math.floor(Math.random() * max);
+    },
+    shuffled = $.map(allElems, function(){
+      var random = getRandom(allElems.length),
+      randEl = $(allElems[random]).clone(true)[0];
+      allElems.splice(random, 1);
+      return randEl;
+    });
+
+    this.each(function(i){
+      $(this).replaceWith($(shuffled[i]));
+    });
+
+    return $(shuffled);
+
+  };
+
+})(jQuery);
+
 $( document ).ready(function() {
+
+    $('.action-item').addClass('gray');
+    $('.action-item').shuffle();
+
     $('.menu-description').hide();
+
+    // click the menu item
 
     $('.menu-item').click(function() {
 
         if ($(this).find('.menu-description').length) {
+
+          $('.action-item').addClass('gray');
+
           $('.menu-description').slideUp();
 
           $(this).find('.menu-description').slideToggle();
@@ -13,18 +47,29 @@ $( document ).ready(function() {
           $('.' + $action).fadeTo('fast',1);
           $('.action-item').not($('.' + $action)).fadeTo('fast', 0.3);
 
-          $('.opaque span').show();
+          $('.opaque span.client').show();
           $('.action-item').removeClass('opaque');
 
+          $('.' + $action).removeClass('gray');
+
           $('.action-item').not($('.' + $action)).addClass('opaque');
-          $('.opaque span').hide();
+          $('.opaque span.client').hide();
         }
         else {
-          $('.opaque span').show();
+          $('.opaque span.client').show();
+
+          $('.action-item').addClass('gray');
+
           $('.action-item').removeClass('opaque');
 
           $('.menu-description').slideUp();
+
+          $action = $(this).find('.menu-description').data('action-type');
+
           $('.' + $action).addClass('opaque');
+
+          $('.' + $action).removeClass('gray');
+
           $('.action-item').fadeTo('fast',1);
         }
 
@@ -68,7 +113,7 @@ $( document ).ready(function() {
 
       $(".pop-up").addClass('animated zoomOutUp');
 
-    }, 5500)
+    }, 10500)
 
     $("body").click(function(){
       $(".pop-up").addClass('animated zoomOutUp');
